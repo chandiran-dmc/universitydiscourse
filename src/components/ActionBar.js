bd310ce0ef72bf0e312e5951f8c74f0684415559
thinkty <thinkty@naver.com>
2020-02-15 14:31:39 -0500

    initial setup for main feed page and top bar UI

---
+++ src/components/ActionBar.js
@@ -0,0 +1,25 @@
+/**
+ * This is a react component class for the action bar which
+ * contains several buttons such as :
+ * - 'Make a post'
+ * - 'Text'
+ * - 'Image'
+ * - 'Document'
+ * - 'Calendar'
+ * - 'Link'
+ * 
+ * Please look at the UI mockup for image explanation
+ */
+
+ import React, { Component } from 'react'
+ 
+ export default class ActionBar extends Component {
+     render() {
+         return (
+             <div>
+                 
+             </div>
+         )
+     }
+ }
+ 
---
+++ src/components/CreatePostPage.js
@@ -0,0 +1,26 @@
+/**
+ * This is a page for creating a post
+ * The user should arrive to this page only if the user clicked
+ * on the 'make a post' button
+ * 
+ * This page should consist of 
+ * - option to choose which type of post the user wants
+ * - post title field
+ * - post content field
+ * - tags field
+ * - cancel and submit button
+ * 
+ * Please look at the UI mockup for image explanation
+ */
+
+import React, { Component } from 'react'
+
+export default class CreatePostPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
