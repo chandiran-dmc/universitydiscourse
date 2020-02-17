34c4809c599ba4a0decffb59d127a9fec1d91896
Drishti <agarw170@purdue.edu>
2020-02-16 19:04:54 -0500

    Create Post Page(Text Post)

--- src/components/CreatePostPage/CreatePostPage.js
+++ src/components/CreatePostPage/CreatePostPage.js
@@ -14,14 +14,71 @@
  */
 
 import React, { Component } from 'react'
+import TopBar from '../TopBar/TopBar';
+import './CreatePostPage.css'
 
 export default class CreatePostPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+    handleSearch(event) {
+        // This is to stop the default behavior, 
+        // which in this case is stopping form from reloading itself
+        event.preventDefault();
+        alert('Searching for ' + this.state.value);
+    }
+    
+    
+
+    handleChange(event) {
+        this.setState(
+            {
+                value: event.target.value
+            }
+        );
+    }
 
     
 
     render() {
         return (
             <div>
+                <TopBar />
+                <div className="CreatePostPage">
+                    <form
+                        onSubmit={this.handleSearch}>
+                     <input
+                        className = "Heading"
+                        type="text"
+                        value={this.state.value}
+                        placeholder="Post Title..."
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        className = "TextBox"
+                        type="text"
+                        value={this.state.value}
+                        placeholder="Content..."
+                        onChange={this.handleChange}
+                    /> 
+                    <input 
+                        className="SubmitButton"
+                        type="submit" 
+                        value="Make Post" 
+                    />
+
+
+                    </form>
+
+
+                </div>
+
                 
             </div>
         )
