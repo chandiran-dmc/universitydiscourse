489a039c98f158bd6e72df97a9510acd55ece248
thinkty <thinkty@naver.com>
2020-02-16 20:01:20 -0500

    fixed redirection

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-import CreatePostPage from './components/CreatePostPage';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CreatePostPage />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
