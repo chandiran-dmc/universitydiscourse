cf48f82297cc5fc17c4adddffd11ecb5a739ada4
chandiran-dmc <dmohanachandiran@gmail.com>
2020-02-23 19:49:20 -0500

    Final changes

--- controllers/user-ctrl.js
+++ controllers/user-ctrl.js
@@ -40,6 +40,45 @@ AuthenticateUser = (req, res) => {
     }).catch(err => console.log(err))
 };
 
+ChangePassword = (req, res) => {
+    const body = req.body
+    const { email, password } = body;
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide an email',
+        })
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!user) {
+            return res
+                .status(404)
+                .json({ success: false, error: `User not found` })
+        } else {            
+            user
+            .isCorrectPassword(password, function(err, same) {
+                if (err) {
+                    res.status(500)
+                    .json({error: 'Internal error please try again'});
+                } else if (!same) {
+                    res.status(401)
+                    .json({error: 'Incorrect email or password'});
+                }
+                else {
+                    return res
+                    .json({ success: false, error: `Password was correct` })
+                }
+            })                         
+        }
+    }).catch(err => console.log(err))
+};
+
 
 
 RegisterUser = (req, res) => {
@@ -78,5 +117,6 @@ RegisterUser = (req, res) => {
 
 module.exports = {
     RegisterUser,
-    AuthenticateUser
+    AuthenticateUser,
+    ChangePassword
 }
--- routes/user-router.js
+++ routes/user-router.js
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.post('/user', UserCtrl.RegisterUser)
 router.post('/authenticate', UserCtrl.AuthenticateUser)
+router.post('/changepass', UserCtrl.ChangePassword)
 
 module.exports = router
