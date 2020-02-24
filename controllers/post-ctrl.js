b75e74326353ec61c807b7215b648c5572f6a22a
thinkty <thinkty@naver.com>
2020-02-23 20:20:49 -0500

    minor update

--- controllers/post-ctrl.js
+++ controllers/post-ctrl.js
@@ -11,7 +11,7 @@ createPost = (req, res) => {
     if (!body) {
         return res.status(400).json({
             success: false,
-            error: 'You must provide a movie',
+            error: 'You must provide a post',
         });
     }
 
