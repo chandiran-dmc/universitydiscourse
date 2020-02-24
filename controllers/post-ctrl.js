90fc74dc791110ccfaaf620d4e8ff1b3efbc298a
thinkty <thinkty@naver.com>
2020-02-23 19:59:51 -0500

    added post create handler

---
+++ controllers/post-ctrl.js
@@ -0,0 +1,133 @@
+const Post = require('../models/post-model');
+
+/**
+ * Function to create a post on the database
+ */
+createPost = (req, res) => {
+
+    // parsing body
+    const body = req.body;
+    // error checking
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a movie',
+        });
+    }
+
+    // create post model
+    const post = new Post(body);
+
+    // error checking if post model was successfully created
+    if (!post) {
+        return res.status(400).json({ success: false, error: err });
+    }
+
+    // saving the post to the database
+    post
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: post.id,
+                message: 'Post created!',
+            });
+        })
+        .catch((error) => {
+            return res.status(400).json({
+                error,
+                message: 'Post not created!',
+            });
+        });
+}
+
+updateMovie = async (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    Movie.findOne({ _id: req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Movie not found!',
+            })
+        }
+        movie.name = body.name
+        movie.time = body.time
+        movie.rating = body.rating
+        movie
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: movie._id,
+                    message: 'Movie updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'Movie not updated!',
+                })
+            })
+    })
+}
+
+deleteMovie = async (req, res) => {
+    await Movie.findOneAndDelete({ _id: req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!movie) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Movie not found` })
+        }
+
+        return res.status(200).json({ success: true, data: movie })
+    }).catch(err => console.log(err))
+}
+
+getMovieById = async (req, res) => {
+    await Movie.findOne({ _id: req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!movie) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Movie not found` })
+        }
+        return res.status(200).json({ success: true, data: movie })
+    }).catch(err => console.log(err))
+}
+
+getMovies = async (req, res) => {
+    await Movie.find({}, (err, movies) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!movies.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Movie not found` })
+        }
+        return res.status(200).json({ success: true, data: movies })
+    }).catch(err => console.log(err))
+}
+
+module.exports = {
+    createMovie,
+    updateMovie,
+    deleteMovie,
+    getMovies,
+    getMovieById,
+}
