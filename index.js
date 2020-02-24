91f134e9bc51f8a97b564c8907e1c598dfda8664
thinkty <thinkty@naver.com>
2020-02-24 13:21:29 -0500

    Added axios example

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const db = require('./db')
 const postRouter = require('./routes/post-router')
+const axios = require('axios');
 
 const app = express()
 const apiPort = 3000
@@ -20,4 +21,28 @@ app.get('/', (req, res) => {
 
 app.use('/api', postRouter)
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(apiPort, () => {
+    console.log(`Server running on port ${apiPort}`)
+
+    axios({
+        method: 'post',
+        url: 'http://localhost:3000/api/post',
+        data: {
+            // Pid should be a unique value by hashing the time + username
+            pid: 400,
+            title: "tae yoon fuck you",
+            user: "tae",
+            type: "text",
+            tag: ["cs240", "cs252"],
+            count: 100,
+            comments: [],
+            content: "fuck you",
+        }
+    })
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+})
--- src/mock_data/example_request.js
+++ src/mock_data/example_request.js
@@ -1,6 +1,20 @@
 const axios = require('axios').default;
 
-axios({
+export default hello = axios({
     method: 'post',
-    url: 'http'
+    url: 'http://localhost:3000/',
+    data: {
+        // Pid should be a unique value by hashing the time + username
+        pid: 400,
+        title: "tae yoon fuck you",
+        user: "tae",
+        type: "text",
+        tag: ["cs240", "cs252"],
+        count: 100,
+        comments: [],
+        content: "fuck you",
+    }
 })
+.then((response) => {
+    console.log(response);
+});
