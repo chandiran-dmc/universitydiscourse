b643ef9c1e87e7cedcb899eb325eea960f38d4d5
thinkty <thinkty@naver.com>
2020-02-28 01:32:16 -0500

    minor fix

--- src/components/MainFeedPage/tests/MainFeedPage.test.js
+++ src/components/MainFeedPage/tests/MainFeedPage.test.js
@@ -3,16 +3,6 @@ import { render } from '@testing-library/react';
 import MainFeedPage from '../MainFeedPage';
 
 
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-
 test('should render actionbar', () => {
     const { findByText } = render(<MainFeedPage/>);
     expect(findByText("create a post") && findByText("text") && findByText("image") && findByText("document") && findByText("calendar") && findByText("link")).toBeTruthy();
@@ -27,4 +17,3 @@ test('should render profile section', () => {
     const { findByText } = render(<MainFeedPage/>);
     expect(findByText("profile") && findByText("change password") && findByText("change email") && findByText("logout")).toBeTruthy();
 });
-
