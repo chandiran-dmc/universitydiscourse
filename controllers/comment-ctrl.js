e1b0ea62f8bfa53c0b8040f513409e5545fdb63a
chandiran-dmc <dmohanachandiran@gmail.com>
2020-03-28 11:06:04 -0400

    Create comment function created

---
+++ controllers/comment-ctrl.js
@@ -0,0 +1,48 @@
+const Comment = require('../models/comment-model');
+
+/**
+ * Function to create a Comment on the database
+ */
+createComment = (req, res) => {
+    console.log("createComment function called");
+
+    // parsing body
+    const body = req.body;
+    // error checking
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a comment',
+        });
+    }
+
+    // create post model
+    const comment = new Comment(body);
+
+    // error checking if post model was successfully created
+    if (!comment) {
+        return res.status(400).json({ success: false, error: err });
+    }
+
+    // saving the post to the database
+    comment
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: comment.id,
+                message: 'Comment created!',
+            });
+        })
+        .catch((error) => {
+            return res.status(400).json({
+                error,
+                message: 'Comment not created!',
+            });
+        });
+}
+
+
+module.exports = {
+    createComment,
+}
