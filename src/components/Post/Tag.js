e736ea38aec99e68570452eb187f03c75da82e4a
thinkty <thinkty@naver.com>
2020-04-10 00:01:27 -0400

    Implemented course page with grade graph

--- src/components/Post/Tag.js
+++ src/components/Post/Tag.js
@@ -4,6 +4,7 @@
 
 import React, { Component } from 'react';
 import { Button, Dialog, DialogTitle, DialogActions, DialogContent } from '@material-ui/core';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
 
@@ -15,8 +16,7 @@ export default class Tag extends Component {
         this.state = {
             name: this.props.name,
             open: false,
-            graph: <div/>,
-            showingGraph: false
+            toCoursePage: false
         };
     }
 
@@ -74,143 +74,16 @@ export default class Tag extends Component {
         });
     }
 
-    /**
-     * This method pulls data from the server and displays the grade
-     */
-    showGraph = async (tag) => {
-
-        // curve (D, C, B, A)
-        let defaultCurves = [90, 80, 75, 70];
-
-        // sample grade occurence (A: 21%, B: 36%, C: 25%, D: 14%, F: 4%)
-        // based on 100 students
-        let occurences = [21, 36, 25, 14, 4];
-
-        // query the database for curves
-        let curves = [];
-        await axios({
-            method: 'get',
-            url: 'http://localhost:3000/api/getcurves'
-        }).then(response => {
-            let temp = response.data.data;
-            // filter the curves so that we only get the curves for the current tag
-            for (let i = 0; i < temp.length; i++) {
-                if (temp[i].course === this.state.name) {
-                    curves.push(temp[i]);
-                }
-            }
-        }).catch(error => {
-            console.error(error);
-        });
-
-        // query the database for grades only if there is no curves
-        let grades = [];
-        if (curves.length === 0) {
-            await axios({
-                method: 'get',
-                url: 'http://localhost:3000/api/getgrades'
-            }).then(response => {
-                let temp = response.data.data;
-                // filter the curves so that we only get the curves for the current tag
-                for (let i = 0; i < temp.length; i++) {
-                    if (temp[i].course === this.state.name) {
-                        grades.push(temp[i]);
-                    }
-                }
-            }).catch(error => {
-                console.error(error);
-            });
-        }
-
-        // the priority of data is "curves" > "grades" > "default"
-        if (curves.length !== 0) {
-            // get the average of each curve
-            // average for A
-            for (let i = 0; i < curves.length; i++) {
-                defaultCurves[0] += Number(curves[i].bound_a);
-            }
-            defaultCurves[0] = defaultCurves[0] / (curves.length + 1);
-            // average for B
-            for (let i = 0; i < curves.length; i++) {
-                defaultCurves[1] += Number(curves[i].bound_b);
-            }
-            defaultCurves[1] = defaultCurves[1] / (curves.length + 1);
-            // average for C
-            for (let i = 0; i < curves.length; i++) {
-                defaultCurves[2] += Number(curves[i].bound_c);
-            }
-            defaultCurves[2] = defaultCurves[2] / (curves.length + 1);
-            // average for D
-            for (let i = 0; i < curves.length; i++) {
-                defaultCurves[3] += Number(curves[i].bound_d);
-            }
-            defaultCurves[3] = defaultCurves[3] / (curves.length + 1);
-
-        } else if (grades.length !== 0) {
-            for (let i = 0; i < grades.length; i++) {
-
-                // increment the occurence based on grade
-                switch (grades[i].grade) {
-                    case "A":
-                        occurences[0]++;
-                        break;
-                
-                    case "B":
-                        occurences[1]++;
-                        break;
-
-                    case "C":
-                        occurences[2]++;
-                        break;
-
-                    case "D":
-                        occurences[3]++;
-                        break;
-
-                    case "F":
-                        occurences[4]++;
-                        break;
-                }
-            }
-        }
-
-        // set options for the chart react component
-        const state = {
-            labels: ["A >" + defaultCurves[0], "B >" + defaultCurves[1], "C >" + defaultCurves[2], "D >" + defaultCurves[3], "F"],
-            datasets: [
-                {
-                    label: "",
-                    fill: true,
-                    lineTension: 0.5,
-                    backgroundColor: 'rgba(75,192,192,1)',
-                    borderColor: 'rgba(0,0,0,1)',
-                    borderWidth: 2,
-                    data: occurences,
-                    pointRadius: 2,
-                }
-            ]
-        };
-
-        const option = {
-            legend: {
-                display: false
-            },
-        };
-        // toggle grade graph
-        if (this.state.showingGraph) {
-            this.setState({
-                graph: <div/>,
-                showingGraph: false
-            });
-        } else {
-            this.setState({
-                graph: <Line data={state} options={option}/>,
-                showingGraph: true
-            });
-        }
-    }
 
     render() {
+
+        // redirect to course page
+        if (this.state.toCoursePage) {
+            return <Redirect exact from="/" push to={{
+                pathname: "/course/" + this.state.name,
+            }}/>;
+        }
+
         return (
             <div>
                 <Button
@@ -243,9 +116,9 @@ export default class Tag extends Component {
                             color="primary"
                             size="small"
                             fullWidth
-                            onClick={() => {this.showGraph(this.state.name)}}
+                            onClick={() => {this.setState({toCoursePage: true})}}
                         >
-                            View Course Graph
+                            Go to course page
                         </Button>
                     </DialogActions>
                     {this.state.graph}
